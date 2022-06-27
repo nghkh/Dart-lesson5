f85588bc3ccdbc22d45b1abcee94a1d1f37fb0ec
nghkh <107782283+nghkh@users.noreply.github.com>
2022-06-27 13:21:31 +0700

    Tối ưu lại code

--- BT-buoi5.dart
+++ BT-buoi5.dart
@@ -3,6 +3,21 @@ void main() {
   Flutter m = Flutter(TenLop: 'FLutter', slhv: 11, HV: hvFlutter);
   m.test();
   m.conlai();
+print('=====================');
+  List<String> hvAndroid = HocVien.hv.sublist(1, 4);
+  Android n = Android(TenLop: 'Android', slhv: 12, HV: hvAndroid);
+  n.test();
+  n.conlai();
+print('=====================');
+  List<String> hvios = HocVien.hv.sublist(3, 6);
+  ios o = ios(TenLop: 'ios', slhv: 13, HV: hvios);
+  o.test();
+  o.conlai();
+print('=====================');
+  List<String> hvweb = HocVien.hv.sublist(5, 6);
+  web p = web(TenLop: 'web', slhv: 14, HV: hvweb);
+  p.test();
+  p.conlai();
 }
 
 class LopHoc {
@@ -10,6 +25,12 @@ class LopHoc {
   final int slhv;
   final List HV;
   const LopHoc({required this.TenLop, required this.slhv, required this.HV});
+  void test() {
+    print('Tên lớp: $TenLop');
+    print('Số Lượng học viên: $slhv');
+    print('Thành viên học viên: $HV');
+  }
+
   void conlai() {
     print('Số lượng học viên còn lại là: ${slhv.reMain(HV.length, slhv)}');
   }
@@ -19,12 +40,6 @@ class Flutter extends LopHoc
     implements buildandroid, buildios, buildweb, buildesktop {
   Flutter({required TenLop, required slhv, required HV})
       : super(TenLop: TenLop, slhv: slhv, HV: HV);
-  void test() {
-    print('Tên lớp: $TenLop');
-    print('Số Lượng học viên: $slhv');
-    print('Thành viên học viên: $HV');
-  }
-
   @override
   void build() {
     print('test');
